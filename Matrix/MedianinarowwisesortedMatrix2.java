d27c62758ea2c290dbeebd7325d3a7c5986fd03b
Anubhav Shukla <43886210+anubhav-shukla@users.noreply.github.com>
2021-10-04 20:23:50 +0530

    Median in a row-wise sorted Matrix
    Median in a row-wise sorted Matrix:-
    Given a row wise sorted matrix of size RxC where R and C are always odd, find the median of the matrix.
    
    It is a solution of gfg problem ...
    Thankyou

---
+++ Matrix/MedianinarowwisesortedMatrix2.java
@@ -0,0 +1,67 @@
+package DSA;
+
+import java.util.Arrays;
+
+public class MedianinarowwisesortedMatrix2 {
+	 static int binaryMedian(int m[][],int r, int c)
+	    {
+	        int max = Integer.MIN_VALUE;
+	        int min = Integer.MAX_VALUE;
+	         
+	        for(int i=0; i<r ; i++)
+	        {
+	             
+	           
+	            if(m[i][0] < min)
+	                min = m[i][0];
+	             
+	            
+	            if(m[i][c-1] > max)
+	                max = m[i][c-1];
+	        }
+	         
+	        int desired = (r * c + 1) / 2;
+	        while(min < max)
+	        {
+	            int mid = min + (max - min) / 2;
+	            int place = 0;
+	            int get = 0;
+	             
+	          
+	            for(int i = 0; i < r; ++i)
+	            {
+	                 
+	                get = Arrays.binarySearch(m[i],mid);
+	                 
+	              
+	                if(get < 0)
+	                    get = Math.abs(get) - 1;
+	                 
+	            
+	                else
+	                {
+	                    while(get < m[i].length && m[i][get] == mid)
+	                        get += 1;
+	                }
+	                 
+	                place = place + get;
+	            }
+	             
+	            if (place < desired)
+	                min = mid + 1;
+	            else
+	                max = mid;
+	        }
+	        return min;
+	    }
+	     
+	
+	    public static void main(String[] args)
+	    {
+	        int r = 3, c = 3;
+	        int m[][]= { {1,3,5}, {2,6,9}, {3,6,9} };
+	         
+	        System.out.println("Median is " + binaryMedian(m, r, c));
+	    }
+	
+}
