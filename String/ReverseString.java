477ff928a65f6eedaea80248cdad08bbf14a5e92
Anubhav Shukla <43886210+anubhav-shukla@users.noreply.github.com>
2021-10-11 19:46:31 +0530

    Reverse String
    Reverse String:-
    Write a function that reverses a string. The input string is given as an array of characters s.
    
     
    it is a leetcode problem hope this solution will help you..
    
    Thankyou.

---
+++ String/ReverseString.java
@@ -0,0 +1,18 @@
+package DSA;
+
+public class ReverseString {
+	 public static void main(String[] args)
+	    {
+	        String input = "Anubhav";
+	 
+	        StringBuilder input1 = new StringBuilder();
+	 
+	   
+	        input1.append(input);
+
+	        input1.reverse();
+	 
+	   
+	        System.out.println(input1);
+	    }
+	}
---
+++ String/ReverseString2.java
@@ -0,0 +1,15 @@
+package DSA;
+
+public class ReverseString2 {
+	public static void main(String[] args)
+    {
+        String str = "Shukla";
+ 
+        
+        StringBuffer sbr = new StringBuffer(str);
+
+        sbr.reverse();
+        System.out.println(sbr);
+    }
+}
+
