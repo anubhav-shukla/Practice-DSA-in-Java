c7a50229b43f342f16b1fa472f5b4e0d109f7317
Anubhav Shukla <43886210+anubhav-shukla@users.noreply.github.com>
2021-10-28 21:23:32 +0530

    Count of number and Find the string in grid
    Count of number of given string in 2D character array:-
    Given a 2-Dimensional character array and a string, we need to find the given string in 2-dimensional character array such that individual characters can be present left to right, right to left, top to down or down to top.
    
    Find the string in grid :-
    Given a 2D grid of n*m of characters and a word, find all occurrences of given word in grid. A word can be matched in all 8 directions at any point. Word is said be found in a direction if all characters match in this direction (not in zig-zag form). The 8 directions are, horizontally left, horizontally right, vertically up, vertically down and 4 diagonal directions.
     
    Hope this solution will help you..
    Thankyou

---
+++ String/Findthestringingrid.java
@@ -0,0 +1,69 @@
+package DSA;
+
+public class Findthestringingrid {
+	 static int R, C;
+	 
+	    static int[] x = { -1, -1, -1, 0, 0, 1, 1, 1 };
+	    static int[] y = { -1, 0, 1, -1, 1, -1, 0, 1 };
+	 
+	  
+	    static boolean search2D(char[][] grid, int row,
+	                            int col, String word)
+	    {
+	        if (grid[row][col] != word.charAt(0))
+	            return false;
+	 
+	        int len = word.length();
+	 
+	        for (int dir = 0; dir < 8; dir++) {
+	      
+	            int k, rd = row + x[dir], cd = col + y[dir];
+	 
+	           
+	            for (k = 1; k < len; k++) {
+	           
+	                if (rd >= R || rd < 0 || cd >= C || cd < 0)
+	                    break;
+	 
+	                if (grid[rd][cd] != word.charAt(k))
+	                    break;
+	 
+	                rd += x[dir];
+	                cd += y[dir];
+	            }
+	 
+	          
+	            if (k == len)
+	                return true;
+	        }
+	        return false;
+	    }
+	 
+	    static void patternSearch(
+	        char[][] grid,
+	        String word)
+	    {
+	       
+	        for (int row = 0; row < R; row++) {
+	            for (int col = 0; col < C; col++) {
+	                if (search2D(grid, row, col, word))
+	                    System.out.println(
+	                        "pattern found at " + row + ", " + col);
+	            }
+	        }
+	    }
+	 
+	    
+	    public static void main(String args[])
+	    {
+	        R = 3;
+	        C = 13;
+	        char[][] grid = { { 'A', 'N', 'B', 'H', 'A', 'V', 'S', 'H', 'U', 'K', 'L', 'A', 'G' },
+	                          { 'A', 'S', 'H', 'I', 'S', 'H', 'S', 'H', 'U', 'K', 'L', 'A', 'G' },
+	                          { 'G', 'O', 'L', 'U', 'B', 'H', 'A', 'I', 'Y', 'A', 'J', 'I', 'G' } };
+	        patternSearch(grid, "ANUBHAV");
+	        System.out.println();
+	        patternSearch(grid, "UUY");
+	    }
+	
+}
