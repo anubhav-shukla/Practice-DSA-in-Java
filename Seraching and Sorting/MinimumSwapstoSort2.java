418cfb2de0080a23d53334cd40b7a12fddebf6f6
Anubhav Shukla <43886210+anubhav-shukla@users.noreply.github.com>
2021-11-14 18:54:51 +0530

    Minimum Swaps to Sort
    Minimum Swaps to Sort :-Given an array of n distinct elements. Find the minimum number of swaps required to sort the array in strictly increasing order.
    
    Hope this solution will help you..
    Thankyou..

---
+++ Seraching and Sorting/MinimumSwapstoSort2.java
@@ -0,0 +1,98 @@
+package DSA;
+
+import java.io.Console;
+import java.util.ArrayList;
+import java.util.Comparator;
+
+import DSA.FindmaxAndMinArray2.Pair;
+
+public class MinimumSwapstoSort2 {
+	   public static int minSwaps(int[] arr)
+	    {
+	        int n = arr.length;
+
+	        // Create two arrays and 
+	        // use as pairs where first
+	        // array is element and second array
+	        // is position of first element
+	        ArrayList <Pair > arrpos =
+	                  new ArrayList <Pair > ();
+	        for (int i = 0; i < n; i++)
+	             arrpos.add(new Pair ());
+
+	        // Sort the array by array element values to
+	        // get right position of every element as the
+	        // elements of second array.
+	        arrpos.sort(new Comparator<Pair>()
+	        {
+	            @Override
+	            public int compare(Pair o1,
+	                               Pair o2)
+	            {
+	                if (o1.getKey() > o2.getKey())
+	                    return -1;
+
+	                // We can change this to make 
+	                // it then look at the
+	                // words alphabetical order
+	                else if (o1.getKey().equals(o2.getKey()))
+	                    return 0;
+
+	                else
+	                    return 1;
+	            }
+	        });
+
+	        // To keep track of visited elements. Initialize
+	        // all elements as not visited or false.
+	        Boolean[] vis = new Boolean[n];
+	        Arrays.fill(vis, false);
+
+	        // Initialize result
+	        int ans = 0;
+
+	        // Traverse array elements
+	        for (int i = 0; i < n; i++)
+	        {
+	            // already swapped and corrected or
+	            // already present at correct pos
+	            if (vis[i] || arrpos.get(i).getValue() == i)
+	                continue;
+
+	            // find out the number of  node in
+	            // this cycle and add in ans
+	            int cycle_size = 0;
+	            int j = i;
+	            while (!vis[j])
+	            {
+	                vis[j] = true;
+
+	                // move to next node
+	                j = arrpos.get(j).getValue();
+	                cycle_size++;
+	            }
+
+	            // Update answer by adding current cycle.
+	            if(cycle_size > 0)
+	            {
+	                ans += (cycle_size - 1);
+	            }
+	        }
+
+	        // Return result
+	        return ans;
+	    }
+	}
+
+	// Driver class
+	class MinSwaps
+	{
+	    // Driver program to test the above function
+	    public static void main(String[] args)
+	    {
+	        int []a = {1, 5, 4, 3, 2};
+	        GfG g = new GfG();
+	        System.out.println(g.minSwaps(a));
+	    }
+	}
+	// This code is contributed by Saksham Seth
