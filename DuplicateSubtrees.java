ddff0586f6d6ee179fedd33a3c06d613f860f88e
Anubhav Shukla <43886210+anubhav-shukla@users.noreply.github.com>
2021-12-05 19:32:13 +0530

    Duplicate Subtrees
    Duplicate Subtrees :-Given a binary tree of size N, your task is to that find all duplicate subtrees from the given binary tree.
    
    Hope this solution will help you//

---
+++ DuplicateSubtrees.java
@@ -0,0 +1,57 @@
+package DSA;
+
+	import java.util.HashMap;
+	public class DuplicateSubtrees {
+
+	    static HashMap<String, Integer> m;
+	    static class Node {
+	        int data;
+	        Node left;
+	        Node right;
+	        Node(int data){
+	            this.data = data;
+	            left = null;
+	            right = null;
+	        }
+	    }
+	    static String inorder(Node node)
+	    {
+	        if (node == null)
+	            return "";
+	     
+	        String str = "(";
+	        str += inorder(node.left);
+	        str += Integer.toString(node.data);
+	        str += inorder(node.right);
+	        str += ")";
+	         
+	        if (m.get(str) != null && m.get(str)==1 )
+	            System.out.print( node.data + " ");
+	     
+	        if (m.containsKey(str))
+	            m.put(str, m.get(str) + 1);
+	        else
+	            m.put(str, 1);
+	        
+	       
+	        return str;
+	    }
+	    static void printAllDups(Node root)
+	    {
+	        m = new HashMap<>();
+	        inorder(root);
+	    } 
+	    public static void main(String args[])
+	    {
+	        Node root = null;
+	        root = new Node(1);
+	        root.left = new Node(2);
+	        root.right = new Node(3);
+	        root.left.left = new Node(4);
+	        root.right.left = new Node(2);
+	        root.right.left.left = new Node(4);
+	        root.right.right = new Node(4);
+	        printAllDups(root);
+	    }
+
+}
