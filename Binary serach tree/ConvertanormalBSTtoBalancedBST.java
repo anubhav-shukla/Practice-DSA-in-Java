a8f35e9b383acbec3be1550f798d96a7bcc343ca
Anubhav Shukla <43886210+anubhav-shukla@users.noreply.github.com>
2021-12-06 15:54:43 +0530

    Convert a normal BST to Balanced BST
    Convert a normal BST to Balanced BST:-Given a BST (Binary Search Tree) that may be unbalanced, convert it into a balanced BST that has minimum possible height.
    
    Hope this solution will help you..

---
+++ Binary serach tree/ConvertanormalBSTtoBalancedBST.java
@@ -0,0 +1,77 @@
+package DSA;
+import java.util.*;
+public class ConvertanormalBSTtoBalancedBST {
+	
+	 
+	static class Node
+	{
+	    int data;
+	    Node left, right;
+	 
+	    public Node(int data)
+	    {
+	        this.data = data;
+	        left = right = null;
+	    }
+	}
+
+	    Node root;
+	
+	    void storeBSTNodes(Node root, Vector<Node> nodes)
+	    {
+	        if (root == null)
+	            return;
+	 
+	        storeBSTNodes(root.left, nodes);
+	        nodes.add(root);
+	        storeBSTNodes(root.right, nodes);
+	    }
+	 
+	    Node buildTreeUtil(Vector<Node> nodes, int start,
+	            int end)
+	    {
+	        if (start > end)
+	            return null;
+	 
+	        int mid = (start + end) / 2;
+	        Node node = nodes.get(mid);
+	 
+	        node.left = buildTreeUtil(nodes, start, mid - 1);
+	        node.right = buildTreeUtil(nodes, mid + 1, end);
+	 
+	        return node;
+	    }
+	 
+	    Node buildTree(Node root)
+	    {
+	        Vector<Node> nodes = new Vector<Node>();
+	        storeBSTNodes(root, nodes);
+	 
+	        int n = nodes.size();
+	        return buildTreeUtil(nodes, 0, n - 1);
+	    }
+	
+	    void preOrder(Node node)
+	    {
+	        if (node == null)
+	            return;
+	        System.out.print(node.data + " ");
+	        preOrder(node.left);
+	        preOrder(node.right);
+	    }
+	    public static void main(String[] args)
+	    {
+	        
+	    	ConvertanormalBSTtoBalancedBST tree = new ConvertanormalBSTtoBalancedBST();
+	        tree.root = new Node(10);
+	        tree.root.left = new Node(8);
+	        tree.root.left.left = new Node(7);
+	        tree.root.left.left.left = new Node(6);
+	        tree.root.left.left.left.left = new Node(5);
+	 
+	        tree.root = tree.buildTree(tree.root);
+	        System.out.println("Preorder traversal of balanced BST is :");
+	        tree.preOrder(tree.root);
+	    }
+	
+}
