5f1a0b39af9d8e930832557e3a2def808cc8b9ef
Anubhav Shukla <43886210+anubhav-shukla@users.noreply.github.com>
2021-12-09 17:43:44 +0530

    Check if it is possible to survive on Island
    Check if it is possible to survive on Island:-You are a poor person in an island. There is only one shop in this island, this shop is open on all days of the week except for Sunday. Consider following constraints: 
    
    N – Maximum unit of food you can buy each day.
    S – Number of days you are required to survive.
    M – Unit of food required each day to survive.
    Currently, it’s Monday, and you need to survive for the next S days. 
    Find the minimum number of days on which you need to buy food from the shop so that you can survive the next S days, or determine that it isn’t possible to survive. 
    
    Hope this solution will help you..

---
+++ Greedy/CheckifitispossibletosurviveonIsland.java
@@ -0,0 +1,28 @@
+package DSA;
+import java.io.*;
+public class CheckifitispossibletosurviveonIsland {
+	static void survival(int S, int N, int M)
+    {
+ 
+     
+        if (((N * 6) < (M * 7) && S > 6) || M > N)
+            System.out.println("No");
+ 
+        else {
+ 
+            int days = (M * S) / N;
+ 
+            if (((M * S) % N) != 0)
+                days++;
+ 
+            System.out.println("Yes " + days);
+        }
+    }
+ 
+    public static void main(String[] args)
+    {
+        int S = 10, N = 16, M = 2;
+ 
+        survival(S, N, M);
+    }
+}
