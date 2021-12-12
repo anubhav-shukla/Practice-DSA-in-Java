e1b83206d077a8e8488f2b88827207a6af2e5c74
Anubhav Shukla <43886210+anubhav-shukla@users.noreply.github.com>
2021-12-12 17:24:03 +0530

    Remove Invalid Parentheses
    Remove Invalid Parentheses:-Given a string s that contains parentheses and letters, remove the minimum number of invalid parentheses to make the input string valid.
    
    Return all the possible results. You may return the answer in any order.
    
    Hope this solution will help you..

---
+++ backtracking/RemoveInvalidParentheses.java
@@ -0,0 +1,71 @@
+package DSA;
+import java.util.*;
+import java.util.LinkedList;
+public class RemoveInvalidParentheses {
+	static boolean isParenthesis(char c)
+	{
+	    return ((c == '(') || (c == ')'));
+	}
+	static boolean isValidString(String str)
+	{
+	    int cnt = 0;
+	    for (int i = 0; i < str.length(); i++)
+	    {
+	        if (str.charAt(i) == '(')
+	            cnt++;
+	        else if (str.charAt(i) == ')')
+	            cnt--;
+	        if (cnt < 0)
+	            return false;
+	    }
+	    return (cnt == 0);
+	}
+	 
+	static void removeInvalidParenthesis(String str)
+	{
+	    if (str.isEmpty())
+	        return;
+	    HashSet<String> visit = new HashSet<String>();
+	    Queue<String> q = new LinkedList<>();
+	    String temp;
+	    boolean level = false;
+	    q.add(str);
+	    visit.add(str);
+	    while (!q.isEmpty())
+	    {
+	        str = q.peek(); q.remove();
+	        if (isValidString(str))
+	        {
+	            System.out.println(str);
+	 
+	            level = true;
+	        }
+	        if (level)
+	            continue;
+	        for (int i = 0; i < str.length(); i++)
+	        {
+	            if (!isParenthesis(str.charAt(i)))
+	                continue;
+	 
+	            temp = str.substring(0, i) + str.substring(i + 1);
+	            if (!visit.contains(temp))
+	            {
+	                q.add(temp);
+	                visit.add(temp);
+	            }
+	        }
+	    }
+	}
+	 
+	public static void main(String[] args)
+	{
+	    String expression = "()())()";
+	    removeInvalidParenthesis(expression);
+	 
+	    expression = "()v)";
+	    removeInvalidParenthesis(expression);
+	}
+	
+       
+
+}
