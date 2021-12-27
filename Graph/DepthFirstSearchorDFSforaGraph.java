9f5f73b134dfdfbb082c314a8733093ea26aaa44
Anubhav Shukla <43886210+anubhav-shukla@users.noreply.github.com>
2021-12-27 19:50:02 +0530

    Depth First Search or DFS for a Graph
    Depth First Search or DFS for a Graph:-Depth First Traversal (or Search) for a graph is similar to Depth First Traversal of a tree. The only catch here is, unlike trees, graphs may contain cycles (a node may be visited twice). To avoid processing a node more than once, use a boolean visited array. 
    
    Hope this solution will help you..

---
+++ Graph/DepthFirstSearchorDFSforaGraph.java
@@ -0,0 +1,61 @@
+package DSA;
+import java.util.*;
+import java.util.LinkedList;
+
+public class DepthFirstSearchorDFSforaGraph {
+	
+	    private int V; 
+	 
+	    private LinkedList<Integer> adj[];
+	 
+	    @SuppressWarnings("unchecked") DepthFirstSearchorDFSforaGraph(int v)
+	    {
+	        V = v;
+	        adj = new LinkedList[v];
+	        for (int i = 0; i < v; ++i)
+	            adj[i] = new LinkedList();
+	    }
+	 
+	    void addEdge(int v, int w)
+	    {
+	        adj[v].add(w); 
+	    }
+	 
+	    void DFSUtil(int v, boolean visited[])
+	    {
+	        visited[v] = true;
+	        System.out.print(v + " ");
+	 
+	        Iterator<Integer> i = adj[v].listIterator();
+	        while (i.hasNext()) {
+	            int n = i.next();
+	            if (!visited[n])
+	                DFSUtil(n, visited);
+	        }
+	    }
+	 
+	    void DFS(int v)
+	    {
+	        boolean visited[] = new boolean[V];
+	 
+	        DFSUtil(v, visited);
+	    }
+	 
+	    public static void main(String args[])
+	    {
+	    	DepthFirstSearchorDFSforaGraph g = new DepthFirstSearchorDFSforaGraph(4);
+	 
+	        g.addEdge(0, 1);
+	        g.addEdge(0, 2);
+	        g.addEdge(1, 2);
+	        g.addEdge(2, 0);
+	        g.addEdge(2, 3);
+	        g.addEdge(3, 3);
+	 
+	        System.out.println(
+	            "Following is Depth First Traversal "
+	            + "(starting from vertex 2)");
+	 
+	        g.DFS(2);
+	    }
+	}
